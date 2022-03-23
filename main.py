ff5fede50cdd41d157d0761819c95135acc25f26
unknown <m.kucha@wp.pl>
2022-03-23 23:45:43 +0100

    :(

--- main.py
+++ main.py
@@ -27,26 +27,25 @@
 
 
 import csv
-
 with open("charts.csv", encoding="utf8") as f:
     csvreader = csv.reader(f)
     title_occurrences = dict()
     for row in csvreader:
-        if (row[0] not in title_occurrences) and not "title":
+        if (row[0] not in title_occurrences) and row[0] != "title":
             title_occurrences[row[0]] = 1
         else:
             if row[0] != "title":
                 title_occurrences[row[0]] += 1
     max_value = 0
-    max_key, min_key = ('', '')
+    max_key, min_key = ('','')
     for key, item in title_occurrences.items():
-        if item > max_value:
-            max_key = key
-            max_value = title_occurrences[max_key]
+            if item > max_value:
+                max_key = key
+                max_value = title_occurrences[max_key]
     min_value = max_value
     for key, item in title_occurrences.items():
-        if item < min_value:
-            min_key = key
-            min_value = title_occurrences[min_key]
+            if item < min_value:
+                min_key = key
+                min_value = title_occurrences[min_key]
     print(f"lowest amount if occ -> {min_key} -> {min_value}\n")
     print(f"highest amount if occ -> {max_key} -> {max_value}\n")
